d1a3fc20809241f4cec81c6c6f2a9487441d259d
AlexCR97 <carp_97@outlook.com>
2020-05-26 10:34:12 -0500

    Working 'snow' engine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MembershipFunction } from './fuzzy-logic-system/membership-functions';
-import { KnowledgeBase } from './fuzzy-logic-system/knowledge-base';
+import { MembershipFunction, MembershipFunctionOutput } from './fuzzy-logic-system/membership-functions';
+import { KnowledgeBase, AirConditionerAction } from './fuzzy-logic-system/knowledge-base';
 import { TemperatureLevel } from './fuzzy-logic-system/temperature-level';
 
 @Component({
@@ -9,24 +9,56 @@ import { TemperatureLevel } from './fuzzy-logic-system/temperature-level';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  
   title = 'fuzzy-logic';
   
+  private tempLevels = [
+    TemperatureLevel.veryCold,
+    TemperatureLevel.cold,
+    TemperatureLevel.warm,
+    TemperatureLevel.hot,
+    TemperatureLevel.veryHot,
+  ];
+
+  public minTemp = 0;
+  public maxTemp = 40;
+  public minTempLevelIndex = 0;
+  public maxTempLevelIndex = this.tempLevels.length - 1;
+
+  public currentTemp = this.maxTemp / 2;
+  public desiredTempLevelIndex = this.maxTempLevelIndex / 2;
+  public desiredTempLevel = this.tempLevels[this.desiredTempLevelIndex];
+
+  public computedTempLevels: MembershipFunctionOutput[];
+  public mostAccurateTempLevel: TemperatureLevel;
+  public acAction: AirConditionerAction;
+
+  public membershipFunction = new MembershipFunction();
+  public knowledgeBase = new KnowledgeBase();
+
   ngOnInit(): void {
-    let m = new MembershipFunction();
-    let k = new KnowledgeBase();
-    let currentTemperature = 40;
-    let targetTemperatureLevel = TemperatureLevel.veryCold;
+    this.computeValues();
+  }
 
-    let output = m.computeOutput(currentTemperature);
+  computeValues() {
+    this.computedTempLevels = this.membershipFunction.computeOutput(this.currentTemp);
+    this.mostAccurateTempLevel = this.computedTempLevels.sort((a, b) => a.percentage - b.percentage)[0].temperatureLevel;
+    this.acAction = this.knowledgeBase.getAirConditionerAction(this.mostAccurateTempLevel, this.desiredTempLevel);
 
-    let currentTemperatureLevel = output.sort((a, b) => a.percentage - b.percentage)[0].temperatureLevel;
+    console.log('Computed temperatre levels are:');
+    console.table(this.computedTempLevels);
 
-    let action = k.getAirConditionerAction(currentTemperatureLevel, targetTemperatureLevel);
+    console.log('Current temperature level is ' + this.currentTemp);
+    console.log('Target temperature level is ' + this.desiredTempLevel);
+    console.log('Air conditionar should ' + this.acAction);
+  }
 
-    console.table(output);
+  onCurrentTempChange(temperature: number) {
+    this.computeValues();
+  }
 
-    console.log('Current temperature level is ' + currentTemperatureLevel);
-    console.log('Target temperature level is ' + targetTemperatureLevel);
-    console.log('Air conditionar should ' + action);
+  onDesiredTempLevelChange(index: number) {
+    this.desiredTempLevel = this.tempLevels[index];
+    this.computeValues();
   }
 }
