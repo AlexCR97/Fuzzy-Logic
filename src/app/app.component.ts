5324bcf0e4ba85fe611f2ee8efbb437a4c7dc516
AlexCR97 <carp_97@outlook.com>
2020-05-26 16:03:32 -0500

    Added dynamic weather engine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,13 +36,43 @@ export class AppComponent implements OnInit {
   public membershipFunction = new MembershipFunction();
   public knowledgeBase = new KnowledgeBase();
 
+  // GUI Variables
+  private backgrounds = new Map<TemperatureLevel, string>([
+    [TemperatureLevel.veryCold, 'bg-very-cold'],
+    [TemperatureLevel.cold, 'bg-cold'],
+    [TemperatureLevel.warm, 'bg-warm'],
+    [TemperatureLevel.hot, 'bg-hot'],
+    [TemperatureLevel.veryHot, 'bg-very-hot'],
+  ]);
+
+  private particles = new Map<TemperatureLevel, string>([
+    [TemperatureLevel.veryCold, 'snow-particle'],
+    [TemperatureLevel.cold, ''],
+    [TemperatureLevel.warm, ''],
+    [TemperatureLevel.hot, ''],
+    [TemperatureLevel.veryHot, 'hot-particle'],
+  ]);
+
+  private layers = new Map<TemperatureLevel, string>([
+    [TemperatureLevel.veryCold, 'snow-layer'],
+    [TemperatureLevel.cold, ''],
+    [TemperatureLevel.warm, ''],
+    [TemperatureLevel.hot, ''],
+    [TemperatureLevel.veryHot, 'hot-layer'],
+  ]);
+
+  public background: string;
+  public particle: string;
+  public layer: string;
+
   ngOnInit(): void {
     this.computeValues();
+    this.updateWeather();
   }
 
   computeValues() {
     this.computedTempLevels = this.membershipFunction.computeOutput(this.currentTemp);
-    this.mostAccurateTempLevel = this.computedTempLevels.sort((a, b) => a.percentage - b.percentage)[0].temperatureLevel;
+    this.mostAccurateTempLevel = this.computedTempLevels.sort((a, b) => b.percentage - a.percentage)[0].temperatureLevel;
     this.acAction = this.knowledgeBase.getAirConditionerAction(this.mostAccurateTempLevel, this.desiredTempLevel);
 
     console.log('Computed temperatre levels are:');
@@ -55,10 +85,17 @@ export class AppComponent implements OnInit {
 
   onCurrentTempChange(temperature: number) {
     this.computeValues();
+    this.updateWeather();
   }
 
   onDesiredTempLevelChange(index: number) {
     this.desiredTempLevel = this.tempLevels[index];
     this.computeValues();
+  }
+
+  updateWeather() {
+    this.background = this.backgrounds.get(this.mostAccurateTempLevel);
+    this.particle = this.particles.get(this.mostAccurateTempLevel);
+    this.layer = this.layers.get(this.mostAccurateTempLevel);
   }
 }
